766cb7c90dbc72fd7e9dc5371eb23911a9d0ccd4
Darshana Pathak <sonal.darshana@gmail.com>
2018-03-10 22:30:48 +1100

    added function to monitor containers' temperature

--- src/temperatureMonitor.js
+++ src/temperatureMonitor.js
@@ -1,12 +1,26 @@
 const temperatureSensor = require('./temperatureSensor');
-const containers = require('./initContainer');
+const data = require('../mockData/data');
 
-// const checkTemperature = () => {
-// 	containers.initializeContainer.id == "Container1"
-// }
-const conatin = containers.initializeContainer.container.id;
-console.log(contain);
-console.log("==========");
-console.log(beers.initializeBeer());
+const beerData = data.getBeerData();
 
+const temperatureMonitor = (containers) =>{
 
+	let stats = {};
+	for (let i = 0; i < containers.length; i++){
+		let container = containers[i].id;
+		let lower = beerData[containers[i].beerType]['temperatureRange']['lower'];
+		let upper = beerData[containers[i].beerType]['temperatureRange']['upper'];
+		let currentTemperature = temperatureSensor();
+		stats[container] = {
+			currentTemperature: currentTemperature,
+			isWithinRange: isWithinRange(lower,upper,currentTemperature)
+		};
+	}
+	return stats;
+}
+
+const isWithinRange = (lower,upper,currentTemperature) => {
+	return (currentTemperature >= lower && currentTemperature <= upper) ? true : false
+}
+
+module.exports = temperatureMonitor;
--- src/temperatureSensor.js
+++ src/temperatureSensor.js
@@ -1,5 +1,7 @@
-module.exports.getCurrentTemperature = () => {
+const getCurrentTemperature = () => {
 	const min = 2;
 	const max = 10;
 	return Math.floor(Math.random() * (max - min +1)) + min;
 }
+
+module.exports = getCurrentTemperature;
